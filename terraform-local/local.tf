edf03981a4aad9379658e443b79dcfdb17e9e3cd
Anurag <achatap@gmail.com>
2022-12-08 12:39:13 +0000

    new changes

---
+++ terraform-local/local.tf
@@ -0,0 +1,4 @@
+resource "local_file" "devops"{
+	filename = "/home/ubuntu/Desktop/devops/terraform-project/terraform-local/devops_automated.txt"
+	content = "THis is my first file usiong terraform" 
+}
